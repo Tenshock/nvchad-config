f0d945cef7ad304751583a876e4723426dc3d51b
Cédric Prezelin <cedric.prezelin-ext@ixxitechside.com>
2023-05-04 12:36:12 +0200

    feat: add folding capabilities

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local opt = vim.opt
+local api = vim.api
 
 local function open_nvim_tree(data)
   -- buffer is a directory
@@ -19,3 +20,9 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 ------------ options ------------
 opt.relativenumber = true
+
+------- folding options ---------
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldenable = false
+opt.foldlevel = 20
